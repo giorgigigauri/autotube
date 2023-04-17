66bfd14c2dbac0b1eba67eb05b75c3f7cbd39869
0awes0me <ramazi2241@gmail.com>
2023-04-17 19:22:21 +0400

    refactos

--- utils/CreateMovie.py
+++ utils/CreateMovie.py
@@ -7,8 +7,21 @@ from mutagen.mp3 import MP3
 from datetime import date
 import time
 import calendar
+from moviepy.editor import VideoFileClip, ColorClip, CompositeVideoClip, ImageClip, TextClip
+import textwrap
 
 dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+def create_rectangle(width, height, color, position):
+    return ColorClip(size=(width, height), color=color).set_position(position)
+
+
+def wrap_text(text, max_width, font, fontsize):
+    wrapper = textwrap.TextWrapper(width=max_width)
+    wrapped_text = wrapper.fill(text=text)
+    return TextClip(wrapped_text, font=font, fontsize=fontsize, color="black")
+
 
 def GetDaySuffix(day):
     if day == 1 or day == 21 or day == 31:
@@ -20,8 +33,10 @@ def GetDaySuffix(day):
     else:
         return "th"
 
+
 dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 music_path = os.path.join(dir_path, "Music/")
+
 
 def add_return_comment(comment):
     need_return = 30
@@ -35,18 +50,20 @@ def add_return_comment(comment):
             return_added += 1
         new_comment += letter
     return new_comment, return_added
+
+
 def getUniqId():
     current_GMT = time.gmtime()
 
     time_stamp = calendar.timegm(current_GMT)
     return str(time_stamp)
 
+
 class CreateMovie():
 
     @classmethod
-    def CreateVideoAudio(self, text, start = 0):
-
-        audioPath = "audio/"+getUniqId()+"tts.mp3"
+    def CreateVideoAudio(self, text, start=0):
+        audioPath = "audio/" + getUniqId() + "tts.mp3"
         tts = gTTS(text=text, lang='en', tld='com.au', slow=False)
         tts.save(audioPath)
         audio = MP3(audioPath)
@@ -54,7 +71,7 @@ class CreateMovie():
 
         music_file = audio
         music = AudioFileClip(audioPath)
-        music = music.set_start(start,int(audio.info.length))
+        music = music.set_start(start, int(audio.info.length))
         music = music.volumex(1)
         music = music.set_duration(int(audio.info.length))
         return music
@@ -62,34 +79,61 @@ class CreateMovie():
     @classmethod
     def CreateVideoText(cls, text, xPos, yPos, start, duration):
         txt = TextClip(text, font='Courier',
-                       fontsize=40, color='#FFF',  bg_color='#00FF00')
+                       fontsize=40, color='#FFF', bg_color='#00FF00')
         txt = txt.on_color(col_opacity=.3)
         txt = txt.set_position((xPos, yPos))
 
-        txt = txt.set_start(0,start)  # (min, s)
+        txt = txt.set_start(0, start)  # (min, s)
         txt = txt.set_duration(duration)
         txt = txt.crossfadein(0.95)
         txt = txt.crossfadeout(0.95)
         return txt
 
-
     @classmethod
     def CreateMP4(cls, post_data):
-        text = post_data['Best_comment']
+        text = post_data['title']
 
-
-        clip = VideoFileClip('minecraft.mp4').subclip(0,20)
-        textClip =  cls.CreateVideoText(text, 10, 50, 0,20)
+        clip = VideoFileClip('minecraft.mp4').subclip(0, 20)
+        clip = cls.AddVideoTitleText(clip, 'reddit-logo.png', text)
+        #todo::
+        # cls.addSubTextsVideo([texts])
         textAudio = cls.CreateVideoAudio(text, 0)
         clip = clip.set_audio(textAudio)
-        clip = CompositeVideoClip([clip, textClip])
         clip.write_videofile(getUniqId() + "output.mp4", fps=24)
         return 'Done'
 
-    def AddVideoTitleText(self):
-    def AddVideoSubtitleText(self):
+    @classmethod
+    def AddVideoTitleText(cls, video, logo, text):
+        video_width, video_height = video.size
+        logo = ImageClip(logo).resize(height=30, width=30)
+        padding = 20
 
-    def GetVideoBackgroundVideo(self):
+        max_text_width = 60
+        title = wrap_text(text, max_text_width, font="Arial", fontsize=12)
+
+        title_width, title_height = title.size
+
+        rectangle_width, rectangle_height = title_width + (padding*2) + logo.size[0], title_height + (padding*2)
+        title_x, title_y = (video_width - rectangle_width) / 2, (video_height - rectangle_height) / 2
+        rectangle_position = (title_x - padding, title_y - padding)
+        rect_x, rect_y = rectangle_position
+        rectangle = create_rectangle(width=rectangle_width, height=rectangle_height, color=(240, 240, 240, 4),
+                                     position=rectangle_position, )
+
+        logo_x = title_x - 40
+        logo_y = title_y + (title_height - logo.size[1]) / 2
+
+        logo_clip = logo.set_duration(video.duration).set_position((logo_x, logo_y))
+        title_clip = title.set_duration(video.duration).set_position((title_x, title_y))
+        rectangle_clip = rectangle.set_duration(video.duration).set_position((rect_x, rect_y))
+
+        return CompositeVideoClip([video, rectangle_clip, logo_clip, title_clip],
+                                  use_bgclip=True)
+
+    # def AddVideoSubtitleText(self):
+    #
+    # def GetVideoBackgroundVideo(self):
+
 
 if __name__ == '__main__':
     print(TextClip.list('color'))
